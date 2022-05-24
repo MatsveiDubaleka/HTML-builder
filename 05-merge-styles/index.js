a392b1abce884bbca38bfdabecd8c657e9b56730
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-05-24 09:53:08 +0300

    fix: delete styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -12,36 +12,14 @@ fs.readdir(stylesPath, { withFileTypes: true }, (error, files) => {
     let trueCSSFiles = file.name.split('.')[1];
     if (trueCSSFiles === 'css') {
       const filePath = path.join(stylesPath, file.name);
-      console.log(filePath);
       const readStream = fs.createReadStream(filePath, {
         encoding: 'utf-8',
       });
       readStream.on('data', (styles) => {
         write.write(styles);
       });
-      // stream.on('readable', () => {
-      //   let data = stream.read();
-      //   console.log(data);
-      //   if (data !== null) {
-      //     console.log(cssArr);
-      //     cssArr.push(data);
-      //     fs.open(path.join(distPath, 'bundle.css'), 'w', (err) => {
-      //       if (err) throw err;
-      //       i = i + 1;
-      //       console.log(cssArr[i]);
-      //       fs.appendFile(
-      //         path.join(distPath, 'bundle.css'),
-      //         cssArr[i],
-      //         (err) => {
-      //           if (err) throw err;
-      //         }
-      //       );
-      //     });
-      //   }
-      // });
-    } else {
-      return;
     }
   });
+  console.log('Styles are merged');
 });
 // создать поток записи, пройтись циклом по файлам, для каждого создавать поток чтения перенаправляя его через pipe в поток записи
