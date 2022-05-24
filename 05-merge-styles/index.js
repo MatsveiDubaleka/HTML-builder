306fecc206892b9b6555a0c55df8a8ccf1f2a5c7
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-05-24 09:46:52 +0300

    fix: fix 05-merge-styles (add styles)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,42 +1,46 @@
 const fs = require('fs');
 const path = require('path');
 
-const distPath = path.join(__dirname, 'project-dist');
+const distPath = path.join(__dirname, 'project-dist', 'bundle.css');
 const stylesPath = path.join(__dirname, 'styles');
 
-const write = fs.createReadStream(distPath, 'utf8');
+const write = fs.createWriteStream(distPath, 'utf8');
 
 fs.readdir(stylesPath, { withFileTypes: true }, (error, files) => {
   if (error) console.error(error);
-  // console.log(files);
-  let cssArr = [];
-  let i = -1;
   files.forEach((file) => {
     let trueCSSFiles = file.name.split('.')[1];
     if (trueCSSFiles === 'css') {
       const filePath = path.join(stylesPath, file.name);
-      const stream = fs.createReadStream(filePath, 'utf-8');
-      // console.log(stream);
-      stream.on('readable', () => {
-        let data = stream.read();
-        console.log(data);
-        if (data !== null) {
-          console.log(cssArr);
-          cssArr.push(data);
-          fs.open(path.join(distPath, 'bundle.css'), 'w', (err) => {
-            if (err) throw err;
-            i = i + 1;
-            console.log(cssArr[i]);
-            fs.appendFile(
-              path.join(distPath, 'bundle.css'),
-              cssArr[i],
-              (err) => {
-                if (err) throw err;
-              }
-            );
-          });
-        }
+      console.log(filePath);
+      const readStream = fs.createReadStream(filePath, {
+        encoding: 'utf-8',
       });
+      readStream.on('data', (styles) => {
+        write.write(styles);
+      });
+      // stream.on('readable', () => {
+      //   let data = stream.read();
+      //   console.log(data);
+      //   if (data !== null) {
+      //     console.log(cssArr);
+      //     cssArr.push(data);
+      //     fs.open(path.join(distPath, 'bundle.css'), 'w', (err) => {
+      //       if (err) throw err;
+      //       i = i + 1;
+      //       console.log(cssArr[i]);
+      //       fs.appendFile(
+      //         path.join(distPath, 'bundle.css'),
+      //         cssArr[i],
+      //         (err) => {
+      //           if (err) throw err;
+      //         }
+      //       );
+      //     });
+      //   }
+      // });
+    } else {
+      return;
     }
   });
 });
